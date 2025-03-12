c51611a6a0462bfaab94557b7a23c6ee6e61b89c
Uriel Campaña <urielcamp@gmail.com>
2025-03-12 12:03:21 -0300

    ejercicio 4 y 5 de bucles for faciles y un ejercicio de resolucion de la secuencia de fibonacci con arrays

--- src/Modulo1/BuclesFor.java
+++ src/Modulo1/BuclesFor.java
@@ -1,5 +1,8 @@
 package Modulo1;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class BuclesFor {
 
     public static void main(String[] args) {
@@ -53,6 +56,50 @@ public class BuclesFor {
         System.out.println("Ejercicio 4");
         System.out.println("------------------------------------------");
         System.out.println();
+
+        //Calcular el factorial de un número ingresado por el usuario.
+
+        int n = 5;
+        int facAux = 1;
+        for (int i = 1; i <= n; i++){
+            facAux *= i;
+        }
+
+        System.out.println(facAux);
+
+
+        System.out.println();
+        System.out.println("------------------------------------------");
+        System.out.println("Ejercicio 5");
+        System.out.println("------------------------------------------");
+        System.out.println();
+
+        //Mostrar la tabla de multiplicar del 5.
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println("tabla del 5: " + " 5 * " + i + " = " + 5*i );
+        }
+
+        System.out.println();
+        System.out.println("------------------------------------------");
+        System.out.println("Ejercicio 6");
+        System.out.println("------------------------------------------");
+        System.out.println();
+
+        //Imprimir los primeros 10 números de la serie Fibonacci.
+
+
+        int[] fib = new int[10];
+        fib[0] = 1;
+        fib[1] = 1;
+
+        int num;
+        for (int i = 2; i < fib.length; i++){
+            num = fib[i -1] + fib[i - 2];
+            fib[i] = num;
+        }
+
+        System.out.println(Arrays.toString(fib));
 
 
 
