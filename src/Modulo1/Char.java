7e5ff996972a53cd65965f4bd740d75947281a42
Uriel Campaña <urielcamp@gmail.com>
2025-03-26 23:03:13 -0300

    ejercicios char faciles

--- src/Modulo1/Char.java
+++ src/Modulo1/Char.java
@@ -53,6 +53,73 @@ public class Char {
             System.out.println(m + " no es una mayuscula");
         }
 
+        System.out.println();
+        System.out.println("-------------------------------");
+        System.out.println();
+        System.out.println("EJERCICIO 4");
+        System.out.println();
+
+        //Verificar si un char es un número.
+
+        char n = '8';
+
+        boolean isNumber = Character.isLetter(n);
+
+        if (isNumber) {
+            System.out.println(n + " No es un numero");
+        }else{
+            System.out.println(n + " Es un numero");
+        }
+
+
+        System.out.println();
+        System.out.println("-------------------------------");
+        System.out.println();
+        System.out.println("EJERCICIO 5");
+        System.out.println();
+
+        //Contar cuántas veces aparece un carácter en una palabra.
+
+        String palabra = "lalalo";
+
+        char a = 'a';
+
+        int count = 0;
+        for(int i = 0; i < palabra.length(); i++){
+            if (palabra.charAt(i) == a) {
+                count++;
+            }
+        }
+
+        System.out.println("En la palabra " + palabra + " aparece el caracter " + a + " " + count + " veces");
+
+        System.out.println();
+        System.out.println("-------------------------------");
+        System.out.println();
+        System.out.println("EJERCICIO 6");
+        System.out.println();
+
+        //Comprobar si un char es un símbolo (@, #, $, etc.).
+
+        char simbol = '&';
+
+        
+        
+        if (simbol == '@' || simbol == '#' || simbol == '$' || simbol == '&') {
+            System.out.println(simbol + " es un simbolo");
+        }else{
+            System.out.println(simbol + " no es un simbolo");
+        }
+
+
+        System.out.println();
+        System.out.println("-------------------------------");
+        System.out.println();
+        System.out.println("EJERCICIO 7");
+        System.out.println();
+
+        //
+
 
     }
 }
